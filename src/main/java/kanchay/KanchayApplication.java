3619134a74b75a3c625183478d5f191404d9f1be
Kevin More Sandoval <kevinjesusmoresandoval@gmail.com>
2026-08-30 18:04:29 -0500

    feat: crear la estructura del proyecto

---
+++ src/main/java/kanchay/KanchayApplication.java
@@ -0,0 +1,13 @@
+package kanchay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanchayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanchayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/kanchay/KanchayApplicationTests.java
@@ -0,0 +1,13 @@
+package kanchay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanchayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
